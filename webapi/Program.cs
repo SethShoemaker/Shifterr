8fe1f3bf8cdd9b7ba018fea7b7ef3f45e71466a7
Seth Shoemaker <Shoemakerseth2003@gmail.com>
2022-11-03 19:54:49 -0400

    Added UserInfoHelperService to DI container

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -35,6 +35,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 );
 
 builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<UserInfoHelperService>();
 
 var app = builder.Build();
 
