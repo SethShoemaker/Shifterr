b119639894de84f2937a946e98713e54df15fb53
Seth Shoemaker <Shoemakerseth2003@gmail.com>
2023-04-23 11:42:04 -0400

    init mauiclient

---
+++ mauiclient/App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace mauiclient;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ mauiclient/App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace mauiclient;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
